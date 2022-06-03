82f1051b40a6b1247f34835e01481c17be543db5
Alibek Kaliyev <abekek01@gmail.com>
2022-06-03 17:10:03 +0000

    basic layout built

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,112 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import CourseCard from '../components/CourseCard';
-import courseList from '../data/courses.json';
+import cseCourseList from '../data/cse_courses.json';
+import busCourseList from '../data/bus_courses.json';
+import mathCourseList from '../data/math_courses.json';
+
+import ReactFlow, { addEdge, ConnectionLineType, useNodesState, useEdgesState } from 'react-flow-renderer';
+import dagre from 'dagre';
+
+import { initialNodes, initialEdges } from '../data/courses.js';
+
+const dagreGraph = new dagre.graphlib.Graph();
+dagreGraph.setDefaultEdgeLabel(() => ({}));
+
+const nodeWidth = 10;
+const nodeHeight = 60;
+
+const getLayoutedElements = (nodes, edges, direction = 'TB') => {
+  const isHorizontal = direction === 'LR';
+  dagreGraph.setGraph({ rankdir: 'TB', nodesep: 60, ranksep: 40, ranker: 'tight-tree'});
+
+  nodes.forEach((node) => {
+    dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+  });
+
+  edges.forEach((edge) => {
+    dagreGraph.setEdge(edge.source, edge.target);
+  });
+
+  dagre.layout(dagreGraph);
+
+  nodes.forEach((node) => {
+    const nodeWithPosition = dagreGraph.node(node.id);
+    node.targetPosition = isHorizontal ? 'left' : 'top';
+    node.sourcePosition = isHorizontal ? 'right' : 'bottom';
+
+    // We are shifting the dagre node position (anchor=center center) to the top left
+    // so it matches the React Flow node anchor point (top left).
+    node.position = {
+      x: nodeWithPosition.x - nodeWidth / 2 + Math.random() / 1000,
+      y: nodeWithPosition.y - nodeHeight / 2,
+    };
+
+    return node;
+  });
+
+  return { nodes, edges };
+};
+
+const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
+  initialNodes,
+  initialEdges
+);
+
+const LayoutFlow = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(layoutedNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(layoutedEdges);
+
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge({ ...params, type: ConnectionLineType.SmoothStep, animated: true }, eds)),
+    []
+  );
+
+  const onLoad = (reactFlowInstance) => {
+    reactFlowInstance.fitView();
+  }
+  
+  const onLayout = useCallback(
+    (direction) => {
+      const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(
+        nodes,
+        edges,
+        direction
+      );
+
+      setNodes([...layoutedNodes]);
+      setEdges([...layoutedEdges]);
+    },
+    [nodes, edges]
+  );
+
+  return (
+    <div className={styles.layoutflow}>
+      {nodes && <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        style={{height: 800, width: '100%'}}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        connectionLineType={ConnectionLineType.SmoothStep}
+        onLoad={onLoad}
+        zoomOnScroll={false}
+        zoomOnPinch={false}
+        zoomOnDoubleClick={false}
+        panOnDrag={false}
+        panOnScroll={false}
+        fitView
+      />
+      }
+    </div>
+  );
+};
+
 
 function Home(props) {
-  console.log(courseList);
-
   return (
     <div className={styles.container}>
       <Head>
@@ -18,12 +117,7 @@ function Home(props) {
 
       <main className={styles.main}>
         <h3 styles={styles.title}>CSB Flowchart</h3>
-
-        <div className={styles.grid}>
-          {courseList.map((course) =>(
-            <CourseCard key={course.title} name={course.title} />
-          ))}
-        </div>
+        <LayoutFlow></LayoutFlow>
       </main>
     </div>
   )
